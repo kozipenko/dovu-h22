84d9f14231f6d9cef0e365795c46c8a9c5701d0a
Kozzy <kozipenko@gmail.com>
2022-04-27 23:13:12 -0400

    [feat] Add project purchase dialog scaffold

--- src/components/Project/ProjectPurchaseDialog.js
+++ src/components/Project/ProjectPurchaseDialog.js
@@ -1,4 +1,4 @@
-import { Modal } from "@mantine/core";
+import { Button, Group, Modal, Progress, Select, TextInput } from "@mantine/core";
 import { useProjects } from "../../store/projects";
 
 export default function ProjectPurchaseDialog() {
@@ -11,7 +11,37 @@ export default function ProjectPurchaseDialog() {
       opened={projects.isPurchaseDialogOpen}
       onClose={projects.closeDialog}
     >
-      <span>Purchase</span>
+      <Progress size="lg" color="green" value={70} />
+
+      <TextInput
+        mt="md"
+        placeholder="Amount"
+        rightSectionWidth={110}
+        rightSection={(
+          <Select
+            zIndex={1000}
+            data={[
+              { value: "dov", label: "DOV" },
+              { value: "tonnes", label: "TONNES" },
+              { value: "eur", label: "🇪🇺 EUR" },
+              { value: "usd", label: "🇺🇸 USD" },
+              { value: "cad", label: "🇨🇦 CAD" },
+              { value: "gbp", label: "🇬🇧 GBP" },
+              { value: "aud", label: "🇦🇺 AUD" }
+            ]}
+            styles={{
+              input: {
+                borderTopLeftRadius: 0,
+                borderBottomLeftRadius: 0
+              }
+            }}
+          />
+        )}
+      />
+
+      <Group position="left" mt="xl">
+        <Button variant="light" color="green">Checkout</Button>
+      </Group>
     </Modal>
   );
 }
